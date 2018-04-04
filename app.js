95f9c80818e59a8a645864e8c5f8d3db62c7f565
Steven Gissubel <steven.gissubel@gmail.com>
2018-04-04 15:49:59 -0400

    added css

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -22,13 +23,19 @@ app.get('/', function(req, res) {
   connection.query(q, function(err, result) {
     if (err) throw err;
     var count = result[0].count
-    // res.send('The count is ' + count);
     res.render('home', {data: count});
   });
 });
 
 app.post('/register', function(req, res) {
-  var email = req.body.email;
+  var person = {
+    email: req.body.email
+  };
+  connection.query('INSERT INTO users SET ?', person, function(err, result) {
+    if(err) throw err;
+    // res.render( ... thank you page );
+    res.redirect('/');
+  });
   
 });
 
